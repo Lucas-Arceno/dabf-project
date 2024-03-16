e52df61f2eddc72dd8d98b2e169a2cd55ee7df9b
Lucas Arceno <pulgalucas1131@gmail.com>
2024-03-16 16:57:38 -0300

    added more tests

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -13,4 +13,35 @@ class ExampleTest extends TestCase
     {
         $this->assertTrue(true);
     }
+    public function test_if_somatory_is_correct(): void 
+    {
+        $this->assertEquals($this->calculate_two_number(5,5), 5+5); 
+    }
+
+    public function test_if_number_is_pair(): void
+    {
+        $this->assertFalse($this->return_true_if_number_is_pair(11));
+    }
+
+    public function test_that_arrays_contain_email(): void 
+    {
+        $faculdades = ["USP","Unicamp","UFPR","ITA","UTFPR"];
+        $this->assertContains("UTFPR", $faculdades);
+    }
+
+    public function calculate_two_number($number1, $number2): int
+    {
+        $result = $number1 + $number2;
+        return $result;
+    }
+
+    public function return_true_if_number_is_pair($number): bool
+    {
+        $isPair = $number%2;
+        if($isPair === 1) {
+            return false;
+        }
+        return true;
+    }
 }
+
